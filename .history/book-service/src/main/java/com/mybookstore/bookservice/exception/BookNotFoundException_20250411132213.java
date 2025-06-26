592250281e37d31d58e40f6d29642f9d95c8af88
Duc Thanh Nguyen <dnguyen@kolb-digital.de>
2025-06-26 17:45:51 +0200

    🧹 Cleanup: Apply .gitignore rules and remove ignored files

--- .history/book-service/src/main/java/com/mybookstore/bookservice/exception/BookNotFoundException_20250411132213.java
+++
@@ -1,7 +0,0 @@
-package com.mybookstore.bookservice.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
--- .history/book-service/src/main/java/com/mybookstore/bookservice/repository/BookRepository_20250411105544.java
+++
@@ -1,7 +0,0 @@
-package com.mybookstore.bookservice.repository;
-
-import com.mybookstore.bookservice.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
